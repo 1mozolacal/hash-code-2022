03328d6c1942ecf63bb8070f969adecf3ea2a580
Nahzilian <nahz3112@gmail.com>
2022-02-24 16:24:42 -0500

    Update output method and add order_based_on_skill function

--- out_put.py
+++ out_put.py
@@ -1,20 +1,40 @@
 
-#handles output
-
-
-
-#iput in the form of list of 'directive'
+# handles output
+# data in the form of list of 'directive'
 # directive is [project <object>,assignments <list>]
 # assignment is [contrib <object>,skill <string>]
 
+from data_types import project, contributor, skill
 
-def write(input,filename):
 
-    for directive in input:
-        project,assignments = directive
+def order_based_on_skill(assignments: list, proj_req: list) -> list:
+    result = []
+    for req in proj_req:
+        temp = [x[0] for x in assignments if x[1] == req]
+        result += temp
 
-        for assignment in assignments:
-            contrib,skill = assignment
+    return result
+        
 
-    with open(filename,'w') as output_file:
-        pass
+def write(data, filename):
+
+    content = ''
+    content += f"{str(len(data))}\n"
+
+    for directive in data:
+        project, assignments = directive
+        
+        # Project name
+        content += f"{project.name}\n"
+        contribs_ordered = " ".join(order_based_on_skill(assignments, project.roles_ordering))
+        content += f"{contribs_ordered}\n"
+
+    with open(filename, 'w') as output_file:
+        output_file.write(content)
+
+
+test_con_1 = contributor("Test 1", [skill("HTML", "3")])
+test_con_2 = contributor("Test 2", [skill("CSS", "3")])
+proj = project("Test Proj", [skill("HTML", "3"), skill("CSS", "3")])
+
+data = [[proj, [[test_con_1, "HTML"], [test_con_2, "CSS"]]]]
