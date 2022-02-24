021816c9caab972909c8ffc6432746d461399b05
1mozola - ubuntu disk <calvin@stuntfactory.com>
2022-02-24 15:33:22 -0500

    output file

---
+++ out_put.py
@@ -0,0 +1,20 @@
+
+#handles output
+
+
+
+#iput in the form of list of 'directive'
+# directive is [project <object>,assignments <list>]
+# assignment is [contrib <object>,skill <string>]
+
+
+def write(input,filename):
+
+    for directive in input:
+        project,assignments = directive
+
+        for assignment in assignments:
+            contrib,skill = assignment
+
+    with open(filename,'w') as output_file:
+        pass
