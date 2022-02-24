091be596625245992b201e0552ce4c9556b9aa8e
dmezhibovski <danmezh@gmail.com>
2022-02-24 15:39:50 -0500

    Fixed calvins typos

--- solution.py
+++ solution.py
@@ -41,7 +41,7 @@ def solve_single_task_queue(input):
 
 def main():
     directory = 'inputFiles/'
-    test_cases = ['a_an_example.in.txt']
+    test_cases = ['a_an_example.in.txt','b_better_start_small.in.txt','c_collaboration.in.txt','d_dense_schedule.in.txt','e_exceptional_skills.in.txt','f_find_great_mentors.in.txt']
     for test in test_cases:
         inputFile = input(directory+test)
         solution = solve_single_task_queue(inputFile)
